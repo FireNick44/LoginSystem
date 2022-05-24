a613f1d2cf54c1e74c9b6986860da20ec6a74510
th3gumm3l <tim.bernhard@me.com>
2022-05-24 21:07:16 +0200

    adding connection to DB

--- mongodb.js
+++ mongodb.js
@@ -1,16 +1,47 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
+
 
 function main()
 {
-    const uri = "mongodb+srv://<vscode>:<sml12345>@ipt6.lovhm.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    const uri = "mongodb+srv://vscode:sml12345@ipt6.lovhm.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
-    client.connect(err => {
-        const collection = client.db("Login").collection("UsersInfo");
-        // perform actions on the collection object
+    try {
+        // Connect to the MongoDB cluster
+        client.connect();
+
+        //createListing();
+
+    } 
+    
+    catch (e) {
+        console.error(e);
+    } 
+    
+    finally {
         client.close();
-    });
+    }
+    
+    /*
+    function createListing(client, newListing){
+        const result = client.db("Login").collection("UsersInfo").insertOne(newListing);
+        console.log(`New listing created with the following id: ${result.insertedId}`);
+    }
 
+    
+    createListing(client,
+        {
+            username: "",
+            email: "",
+            password: ""
+        }
+        );
+        
+    */
+    
 }
 
-main();
+
+    main();
+    
+    
