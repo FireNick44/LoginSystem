698dedf95c7347610607706a31e2c6a06f8e06ad
th3gumm3l <tim.bernhard@me.com>
2022-05-24 21:55:25 +0200

    ERROR

--- mongodb.js
+++ mongodb.js
@@ -1,47 +1,35 @@
 const { MongoClient } = require('mongodb');
+var server = require('./server', { root: __dirname});
 
+var username = "test";
+var email = "test@gugu.ch";
+var password = "test";
 
-function main()
-{
-    const uri = "mongodb+srv://vscode:sml12345@ipt6.lovhm.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const uri = "mongodb+srv://vscode:sml12345@ipt6.lovhm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
-    try {
-        // Connect to the MongoDB cluster
-        client.connect();
-
-        //createListing();
-
-    } 
-    
-    catch (e) {
-        console.error(e);
-    } 
-    
-    finally {
-        client.close();
-    }
-    
-    /*
-    function createListing(client, newListing){
-        const result = client.db("Login").collection("UsersInfo").insertOne(newListing);
-        console.log(`New listing created with the following id: ${result.insertedId}`);
-    }
-
-    
+try {
+    // Connect to the MongoDB cluster
+    client.connect();
+        
     createListing(client,
         {
-            username: "",
-            email: "",
-            password: ""
+            username: username,
+            email: email,
+            password: password
         }
         );
-        
-    */
+} 
     
+catch (e) {
+    console.error(e);
+} 
+
+finally {
+    client.close();
 }
 
 
-    main();
-    
-    
+function createListing(client, newListing){
+    const result = client.db("Login").collection("UsersInfo").insertOne(newListing);
+}
