de7e32a590c7962bf604b268fc995ca3b68653c4
th3gumm3l <tim.bernhard@me.com>
2022-05-24 22:17:31 +0200

    bitzli fixed

--- mongodb.js
+++ mongodb.js
@@ -5,34 +5,46 @@ var username = "test";
 var email = "test@gugu.ch";
 var password = "test";
 
-const uri = "mongodb+srv://vscode:sml12345@ipt6.lovhm.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
-try {
-    // Connect to the MongoDB cluster
-    client.connect();
-    
-    client.db("Login").collection("UsersInfo").insertOne({username: username, email: email, password: password});
+async function connection(){
+    const uri = "mongodb+srv://vscode:sml12345@ipt6.lovhm.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
-    /*
-    createListing(client,
-        {
-            username: username,
-            email: email,
-            password: password
-        }
+    try 
+    {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        await writeinDatabase(client,
+            {
+                username: username,
+                email: email,
+                password: password
+            }
         );
-    */
-} 
-    
-catch (e) {
-    console.error(e);
-} 
 
-finally {
-    client.close();
+    } 
+
+    catch (e) 
+    {
+        console.error(e);
+    } 
+
+    finally 
+    {
+        client.close();
+    }
+
 }
 
+connection();
+
+
+async function writeinDatabase(client, Liste){
+    client.db("Login").collection("UsersInfo").insertOne(Liste);
+}
+
+    
 /*
 function createListing(client, newListing){
     const result = client.db("Login").collection("UsersInfo").insertOne(newListing);
